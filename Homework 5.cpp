5892f6d5824e14debcc1f945ed1a623e11707ccc
peter-051249 <annaforschool728@gmail.com>
2025-12-16 16:14:55 +0800

    building the structure

--- Homework 5.cpp
+++ Homework 5.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
+#include <stdbool.h>
 using namespace std;
 
+
+typedef struct Pokemon {
+    int ID;
+    string name;
+    string type1;
+    string type2;
+    int total;
+    int hp;
+    int attack;
+    int defence;
+    int spAttack;
+    int spDefence;
+    int speed;
+    int gen;
+    bool legend;
+} Pokemon;
+
+
+
+
+
+
+
 int main () {
-    cout << "Hello World";
-    cout << "Good day";
-    return 0;
+    int comm;
+    while (1) {
+        cout << "*** (^_^) Data Structure (^o^) ***\n";
+        cout << "\n";
+
+
+
+        cin >> comm;
+
+        if (comm == 0) {
+            break; // 暫時
+        }
+
+        else if (comm == 1) {
+
+        }
+    }
+ 
+
 }
